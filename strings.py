fd36b6d670905d96b021bd1512ec4303a2bfe2a1
Leandro Lopes <lopes.ctrlf5@gmail.com>
2024-07-20 17:49:10 -0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,34 @@
+# EXERCICIO 1
+
+# cpf = str(input('Digite seu CPF: '))
+# if len(cpf) == 11 and cpf.isnumeric:
+#     print('CPF cadastrado com sucesso.')
+# else:
+#     print('Digite CPF válido.')
+
+
+# EXERCICIO 2
+
+# cpf = str(input('Digite seu CPF: '))
+# cpf = cpf.replace('  ', ' ')
+# cpf = cpf.strip()
+# cpf = cpf.replace('-', '')
+# cpf = cpf.replace('.', '')
+    
+# if len(cpf) == 11 and cpf.isnumeric:
+#     print('CPF cadastrado com sucesso.')
+# else:
+#     print('Digite CPF válido.')
+
+
+# EXERCICIO 3
+
+# email = input('Digite seu email: ')
+# if '@' in email:
+#     posição = email.find('@')
+#     if '.com' in email[posição:]:
+#         print('{} cadastrado com sucesso.'.format(email))
+#     else:
+#         pass
+# else:
+#         print('Digite email válido.')
